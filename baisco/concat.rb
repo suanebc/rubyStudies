4e4edf8d94cafc7244f02a4415f4d0dc0b1ac456
suanebc <60412898+suanebc@users.noreply.github.com>
2023-02-17 16:46:00 -0300

    Novos conhecimentos

---
+++ baisco/concat.rb
@@ -0,0 +1,12 @@
+puts 9 << 2
+palavra = 'Olá '
+outra_palavra = 'Mundo Vitual!'
+palavra << outra_palavra
+puts palavra
+#puts 9 << 4
+
+
+def odd_or_even(number)
+  number.even?
+end
+  
---
+++ baisco/entrada_saida.rb
@@ -0,0 +1,5 @@
+puts 'Olá'<< ' Mundo! '
+puts 'Olá Mundo!'
+frase = ''
+frase << 'Olá Mundo, de novo!'
+puts frase
